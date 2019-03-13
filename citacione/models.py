0931fd048cc6857065b4dee412cca006ec103437
TUYISENGE Anabella <bellaxbx1109@gmail.com>
2019-03-13 12:04:41 +0200

    add foreign key linking image and location

--- citacione/models.py
+++ citacione/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 import datetime as dt
 
-class Image(models.Model):
-    image=models.ImageField(upload_to = 'images/')
-    name= models.CharField(max_length =30)
-    description=models.TextField()
 
 class Location(models.Model):
      country=models.CharField(max_length=50)
      city=models.CharField(max_length=50)
+
+class Image(models.Model):
+    image=models.ImageField(upload_to = 'images/')
+    name= models.CharField(max_length =30)
+    description=models.TextField()
+    location=models.ForeignKey(Location)
+
 
 
 # Create your models here.

0ce20d30c17d1cbbbc6006a41b948553df680193
TUYISENGE Anabella <bellaxbx1109@gmail.com>
2019-03-13 11:29:26 +0200

    add route and view function to display images

--- citacione/urls.py
+++ citacione/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
+    url('^today/$',views.pic_of_day,name='picToday')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)

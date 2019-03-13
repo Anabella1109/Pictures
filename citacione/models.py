cd59429470ec3965bb5bd6ef65b5925653e2832a
TUYISENGE Anabella <bellaxbx1109@gmail.com>
2019-03-13 11:19:56 +0200

    add Image model in models.py

--- citacione/models.py
+++ citacione/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+import datetime as dt
+
+class Image(models.Model):
+    image=models.ImageField(upload_to = 'images/')
+    name= models.CharField(max_length =30)
+    description=models.TextField()
+
 
 # Create your models here.

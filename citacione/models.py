97274a10ffa9a981e077aed2039bfbecf8caea77
TUYISENGE Anabella <bellaxbx1109@gmail.com>
2019-03-13 12:33:20 +0200

    add foreign keys to image model and create category model

--- citacione/models.py
+++ citacione/models.py
@@ -5,12 +5,17 @@ import datetime as dt
 class Location(models.Model):
      country=models.CharField(max_length=50)
      city=models.CharField(max_length=50)
+     place=models.CharField(max_length=50,default='unkown',null=True)
+
+class Category(models.Model):
+     category=models.CharField(max_length=50)
 
 class Image(models.Model):
     image=models.ImageField(upload_to = 'images/')
     name= models.CharField(max_length =30)
     description=models.TextField()
-    location=models.ForeignKey(Location)
+    location=models.ForeignKey(Location,null=True)
+    category=models.ForeignKey(Category, null=True)
 
 
 

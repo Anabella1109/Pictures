b48e330cfe7bd19b12a7b665baa9038f51948fb7
TUYISENGE Anabella <bellaxbx1109@gmail.com>
2019-03-13 11:41:17 +0200

    add first template

--- Slika/settings.py
+++ Slika/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'citacione.apps.CitacioneConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- citacione/urls.py
+++ citacione/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
-    url('^today/$',views.pic_of_day,name='picToday')
+    url('^today/$',views.pic_of_day,name='picToday'),
+      # url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_pics,name = 'pastPics') ] 
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
